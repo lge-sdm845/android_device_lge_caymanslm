514fed46f4a39e4c3b3d3219d14f7524d61e8bda
Andrey Sprynga <rtx4d@aospa.co>
2025-05-07 00:31:46 +0700

    caymanslm: Move Rescontrol sepolicy to common
    Change-Id: Ib6902b13070aa503d728356bae6d61e7b299a35f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,9 +37,6 @@ TARGET_KERNEL_CONFIG := lineageos_caymanslm_defconfig
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml
