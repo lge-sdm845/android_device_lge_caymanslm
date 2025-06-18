5d44814f8448561dea60cdc993f14b5d9b523f56
Tuan Anh <tuan73176@gmail.com>
2025-06-18 11:04:11 +0700

    caymanslm: Build LGE AIDL Vibrator HAL
    Change-Id: I227db0284625503fb92a5fbaf07792a8ae47fd41

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.lge
+
 # Wi-Fi Overlay
 PRODUCT_PACKAGES += \
     WifiOverlayVELVET
