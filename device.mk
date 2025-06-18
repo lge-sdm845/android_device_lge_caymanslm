5bbfa904c31eb39d0984a220bfc283970586c634
Tuan Anh <tuan73176@gmail.com>
2025-06-18 11:08:25 +0700

    caymanslm: Copy fstab from common
    Change-Id: I739f40a71e6d60b3b989457e4655fc8569aba066

--- device.mk
+++ device.mk
@@ -41,6 +41,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.lge
 
+# Init
+PRODUCT_COPY_FILES += \
+    device/lge/sdm845-common/rootdir/etc/fstab.dynamic:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.caymanslm \
+    device/lge/sdm845-common/rootdir/etc/fstab.dynamic:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.caymanslm
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
